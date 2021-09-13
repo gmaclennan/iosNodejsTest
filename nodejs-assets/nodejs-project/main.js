0c1af53747eb21984bcf4b1891fbf686e029e9f0
Gregor MacLennan <gmaclennan@digital-democracy.org>
2021-09-13 18:04:07 +0100

    feat: Add level to nodejs-mobile

--- nodejs-assets/nodejs-project/main.js
+++ nodejs-assets/nodejs-project/main.js
@@ -2,11 +2,22 @@
 // The main.js file will be overwritten in updates/reinstalls.
 
 var rn_bridge = require('rn-bridge');
+var level = require('level');
+
+var dataDir = rn_bridge.app.datadir();
+
+var db = level(dataDir + '/db');
 
 // Echo every message received from react-native.
-rn_bridge.channel.on('message', (msg) => {
-  rn_bridge.channel.send(msg);
-} );
+rn_bridge.channel.on('message', async (msg) => {
+  const value = await db.get('name');
+  rn_bridge.channel.send(msg + ' value from db: ' + value);
+});
 
-// Inform react-native node is initialized.
-rn_bridge.channel.send("Node was initialized.");
+db.put('name', 'Level', function (err) {
+  if (err) return console.log('Ooops!', err); // some kind of I/O error
+  // Inform react-native node is initialized.
+  rn_bridge.channel.send(
+    'Node was initialized.' + err ? 'Error: ' + err.message : '',
+  );
+});
